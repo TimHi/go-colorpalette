50b65892f6e3e1ea0361ac82ff27c71b85955067
TimHi <TimHiller@live.de>
2024-03-10 22:18:54 +0100

    add cors

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/timhi/golorpalette/src"
 )
 
 func main() {
 
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	e.GET("/colors/:url", func(c echo.Context) error {
 		url := c.Param("url")
+
 		colors, err := src.GetColors(url)
 		if err != nil {
 			return c.String(http.StatusInternalServerError, err.Error())
