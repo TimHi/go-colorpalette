09d69aea9aeef745ef34ea47a1b03d07c11ddcc4
timhiller@live.de <timhiller.live.de>
2024-03-16 11:54:55 +0000

    cors

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	}))
 	e.GET("/colors/:url", func(c echo.Context) error {
 		url := c.Param("url")
